d9a4c215b112c83261a2e66ee2103a022f3fd76a
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-09-17 18:26:46 +0300

    change VC

---
+++ New Balance/MenuViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuViewController.swift
+//  New Balance
+//
+//  Created by Антон Павлов on 17.09.2022.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}
