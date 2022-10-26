730fb0f759ece88058f1ca722e3ca90b6a744881
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-10-26 10:10:14 +0300

    add functions, add new class, add new code

---
+++ New Balance/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  New Balance
+//
+//  Created by Антон Павлов on 25.10.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
--- New Balance/ViewController.swift
+++ New Balance/ViewController.swift
@@ -5,18 +5,60 @@
 //  Created by Антон Павлов on 18.09.2022.
 //
 
-import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
     
     @IBOutlet weak var table: UITableView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    struct Sunset {
+        
+        let title: String
+        let imageName: String
     }
     
+    let data: [Sunset] = [Sunset(title: "New Balance", imageName: "1"),
+                           Sunset(title: "New Balance", imageName: "2"),
+                           Sunset(title: "New Balance", imageName: "3"),
+                           Sunset(title: "New Balance", imageName: "4"),
+                           Sunset(title: "New Balance", imageName: "5"),
+                           Sunset(title: "New Balance", imageName: "6"),
+                           Sunset(title: "New Balance", imageName: "7"),
+                           Sunset(title: "New Balance", imageName: "8"),
+                           Sunset(title: "New Balance", imageName: "9"),
+                           Sunset(title: "New Balance", imageName: "10"),
+                           Sunset(title: "New Balance", imageName: "11"),
+                           Sunset(title: "New Balance", imageName: "12"),
+                           Sunset(title: "New Balance", imageName: "13"),]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        table.dataSource = self
+        table.delegate = self
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let sunset = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = sunset.title
+        cell.iconImageView.image = UIImage(named: sunset.imageName)
+        return cell
+    }
+    
+    // Функция отвечает за размер ячейки
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
 }
+    
+    
+    
+    
+    
+
+   
