fbd614bacfa5d424f3790c773587577d60a6836a
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-10-28 15:28:18 +0300

    Структурировал файлы

---
+++ ViewControllers/InfoAboutShoesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoAboutShoesViewController.swift
+//  New Balance
+//
+//  Created by Антон Павлов on 28.10.2022.
+//
+
+import UIKit
+
+class InfoAboutShoesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
