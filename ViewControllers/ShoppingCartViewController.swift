68db73f66effac7e04683fa9e31a00867156cb35
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-11-04 18:26:04 +0300

    add alert

--- ViewControllers/ShoppingCartViewController.swift
+++ ViewControllers/ShoppingCartViewController.swift
@@ -16,8 +16,14 @@ class ShoppingCartViewController: UIViewController {
 
     
     @IBAction func buttonToDecoration(_ sender: UIButton) {
-       
-       
+        let alert = UIAlertController(title: "", message: "Убедитесь что всё верно добавили в заказ перед тем, как подтвердить его! В течении 5 минут с ваши свяжется наш менеджер для уточнения деталей", preferredStyle: .alert)
+        let okBtn = UIAlertAction(title: "Подтвердить", style: .default, handler: nil)
+        let backBtn = UIAlertAction(title: "Назад", style: .cancel, handler: nil)
+        alert.addAction(okBtn)
+        alert.addAction(backBtn)
+        
+        
+        present(alert, animated: true, completion: nil)
     }
     
 }
