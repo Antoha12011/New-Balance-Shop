d8629869ccb6fb53aa4138fa05febb5b2fe025e8
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-11-29 18:28:52 +0300

    add new price for shoes

--- ViewControllers/TableViewController.swift
+++ ViewControllers/TableViewController.swift
@@ -20,43 +20,43 @@ class TableViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     public func initList() {
-        let dataShoes = Shape(bigText: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu", name: "New Balance 534", imageName: "1", price: "19.999₽")
+        let dataShoes = Shape(bigText: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu", name: "New Balance 574", imageName: "1", price: "19.999₽")
         shapeList.append(dataShoes)
 
         let dataShoes1 = Shape(bigText: "", name: "New Balance 997", imageName: "2", price: "19.999₽")
         shapeList.append(dataShoes1)
 
-        let dataShoes2 = Shape(bigText: "", name: "New Balance", imageName: "3", price: "19.999₽")
+        let dataShoes2 = Shape(bigText: "", name: "New Balance 574", imageName: "3", price: "19.999₽")
         shapeList.append(dataShoes2)
 
-        let dataShoes3 = Shape(bigText: "", name: "New Balance", imageName: "4", price: "19.999₽")
+        let dataShoes3 = Shape(bigText: "", name: "New Balance 574", imageName: "4", price: "19.999₽")
         shapeList.append(dataShoes3)
 
-        let dataShoes4 = Shape(bigText: "", name: "New Balance", imageName: "5", price: "19.999₽")
+        let dataShoes4 = Shape(bigText: "", name: "New Balance 327", imageName: "5", price: "19.999₽")
         shapeList.append(dataShoes4)
 
-        let dataShoes5 = Shape(bigText: "", name: "New Balance", imageName: "6", price: "19.999₽")
+        let dataShoes5 = Shape(bigText: "", name: "New Balance 327", imageName: "6", price: "19.999₽")
         shapeList.append(dataShoes5)
 
-        let dataShoes6 = Shape(bigText: "", name: "New Balance", imageName: "7", price: "19.999₽")
+        let dataShoes6 = Shape(bigText: "", name: "New Balance 574", imageName: "7", price: "19.999₽")
         shapeList.append(dataShoes6)
 
-        let dataShoes7 = Shape(bigText: "", name: "New Balance", imageName: "8", price: "19.999₽")
+        let dataShoes7 = Shape(bigText: "", name: "New Balance 574", imageName: "8", price: "19.999₽")
         shapeList.append(dataShoes7)
 
-        let dataShoes8 = Shape(bigText: "", name: "New Balance", imageName: "9", price: "19.999₽")
+        let dataShoes8 = Shape(bigText: "", name: "New Balance 237", imageName: "9", price: "19.999₽")
         shapeList.append(dataShoes8)
 
-        let dataShoes9 = Shape(bigText: "", name: "New Balance", imageName: "10", price: "19.999₽")
+        let dataShoes9 = Shape(bigText: "", name: "New Balance 574", imageName: "10", price: "19.999₽")
         shapeList.append(dataShoes9)
         
-        let dataShoes10 = Shape(bigText: "", name: "New Balance", imageName: "11", price: "19.999₽")
+        let dataShoes10 = Shape(bigText: "", name: "New Balance 574", imageName: "11", price: "19.999₽")
         shapeList.append(dataShoes10)
 
-        let dataShoes11 = Shape(bigText: "", name: "New Balance", imageName: "12", price: "19.999₽")
+        let dataShoes11 = Shape(bigText: "", name: "New Balance ", imageName: "12", price: "19.999₽")
         shapeList.append(dataShoes11)
 
-        let dataShoes12 = Shape(bigText: "", name: "New Balance", imageName: "13", price: "19.999₽")
+        let dataShoes12 = Shape(bigText: "", name: "New Balance 997", imageName: "13", price: "19.999₽")
         shapeList.append(dataShoes12)
         
         
